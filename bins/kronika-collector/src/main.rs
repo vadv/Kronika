ef0d0c6a7ad591bc3ff6097d2ff49ffe3c71ba2e
dmitrivasilyev <vadv.mkn@gmail.com>
2026-08-10 08:19:26 +0000

    Use jemalloc in static collector builds

--- bins/kronika-collector/src/main.rs
+++ bins/kronika-collector/src/main.rs
@@ -16,6 +16,10 @@
     reason = "the registry's arrow/parquet stack pulls duplicate transitive versions outside our control"
 )]
 
+#[cfg(target_env = "musl")]
+#[global_allocator]
+static GLOBAL_ALLOCATOR: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 mod buffering;
 mod capacity;
 mod config;
